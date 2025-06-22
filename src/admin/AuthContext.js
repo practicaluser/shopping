213677c15fdc17c15a86ee4103c45cf440401a87
practicaluser <dlcnsdncjswo@naver.com>
2025-06-22 22:05:24 +0900

    관리자 로그인2

--- src/admin/AuthContext.js
+++ src/admin/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react'
-import axios from 'axios'
+import axios from '../api/axios'
 
 // 컨텍스트 생성
 const AuthContext = createContext()
